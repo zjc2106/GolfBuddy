81a2274914c0b37eda1456e42abafe59f97ac4e0
Zac Coeur <coeurzac@gmail.com>
2022-07-31 13:01:22 -0500

    Initial Commit

---
+++ GolfTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GolfTracker
+//
+//  Created by Zac Coeur on 7/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
